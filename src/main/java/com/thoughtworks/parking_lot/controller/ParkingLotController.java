4a827a0ef288a787c50847ee6ace62f05d7f5785
LIUST3 <stefan.liu@oocl.com>
2019-07-18 17:37:38 +0800

    [stefan] init : test and repository

--- src/main/java/com/thoughtworks/parking_lot/controller/ParkingLotController.java
+++ src/main/java/com/thoughtworks/parking_lot/controller/ParkingLotController.java
@@ -1,12 +1,15 @@
 package com.thoughtworks.parking_lot.controller;
 
 import com.thoughtworks.parking_lot.model.ParkingLot;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.thoughtworks.parking_lot.repository.ParkingLotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ParkingLotController {
+    @Autowired
+    private ParkingLotRepository parkingLotRepository;
 
     @PostMapping("/parking-lot")
     public ParkingLot save(ParkingLot parkingLot){
